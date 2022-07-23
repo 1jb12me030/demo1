11439818b3e6a7dae5dc2ccdc2310494c499c666
mayank kumar <80468358+1jb12me030@users.noreply.github.com>
2022-07-23 16:41:28 +0530

    6th commit

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from testapp.models import *
 
 # Register your models here.
+admin.site.register(GameCategory)
+
+admin.site.register(Game)
+admin.site.register(Player)
+admin.site.register(PlayerScore)
+
